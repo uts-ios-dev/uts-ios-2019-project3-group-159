1a279870c4ad0674a9090f4db544e462a0f834e5
Andres Amarilis <andresamarilis@Punani-Air-79.local>
2019-05-23 19:07:27 +1000

    add basic xcode template file

---
+++ photoedit/photoedit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  photoedit
+//
+//  Created by Andres Amarilis on 23/5/19.
+//  Copyright © 2019 Andres Amarilis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
